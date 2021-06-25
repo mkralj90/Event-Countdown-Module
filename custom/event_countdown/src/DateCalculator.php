f672863ccfe435b292a4d8d8c4b5cf4aa5a65b3e
mkralj90 <69726885+mkralj90@users.noreply.github.com>
2021-06-25 11:42:30 +0200

    Revision

--- custom/event_countdown/src/DateCalculator.php
+++ custom/event_countdown/src/DateCalculator.php
@@ -2,32 +2,52 @@
 
 namespace Drupal\event_countdown;
 
+use DateTimeZone;
+use Drupal\Core\Datetime\DrupalDateTime;
+
 class dateCalculator{
 
 
-  public function EventCountdown($date) {
-    $difference = $this->Difference($date);
-    // future event
-    if($difference >= 1) {
-      return $difference . " days left until event starts";
-      // same day event
-    } else if($difference == 0) {
-      return "This event is happening today";
-      // past event
-    } else{
-      return "This event already passed " . abs($difference) . " days ago" ;
+    public function EventCountdown($date) {
+        $difference = $this->Difference($date);
+
+        if($difference == 1){
+            $days = "day";
+        }elseif($difference == -1){
+            $days = "day";
+        }else{
+            $days = "days";
+        }
+
+        // future event
+        if($difference >= 1) {
+            return $difference . " " . $days . " left until event starts";
+            // same day event
+        } elseif($difference == 0) {
+            return "This event is happening today";
+            // past event
+        } else{
+            return "This event already passed " .abs($difference) . " " . $days .  " ago" ;
+        }
+
     }
 
-  }
+    // difference between now and event date
+    public function Difference($date) {
 
+        $now = new DrupalDateTime();
+        $now->setTime(0,0);
+        $time_now = $now->getTimestamp();
 
-  // difference between now and event date
-  public function Difference($date) {
+        $event_date = new DrupalDateTime($date);
+        $event_date->setTime(0,0);
+        $time_event = $event_date->getTimestamp();
 
-    $now = time();
-    $event_date = strtotime($date);
-    $difference = $event_date - $now;
-    return round($difference / (60 * 60 * 24));
-  }
+        $difference = $time_event - $time_now;
+
+        return $difference / 86400;
+
+    }
 
 } // end of class
+
